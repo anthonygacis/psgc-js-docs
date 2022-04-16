00541852007d7c2c81629605438bbaf1a92673d0
Anthony Gacis (@ageesea) <anthony.gacis@yahoo.com>
2022-04-16 23:24:36 +0800

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
 	integrations: [tailwind()],
-	site: 'https://anthonygacis.github.io/',
-	base: "/psgc-js-docs"
+	site: 'https://anthonygacis.github.io',
+	base: "/psgc-js-docs/",
 });
