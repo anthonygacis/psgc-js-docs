f0ce7e2b0ccdeef047f46f13c137543d9b97c858
Anthony Gacis (@ageesea) <anthony.gacis@yahoo.com>
2022-04-17 11:57:41 +0800

    Remove site and base

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,5 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-	integrations: [tailwind()],
-	site: 'https://anthonygacis.github.io',
-	base: "/psgc-js-docs/",
+	integrations: [tailwind()]
 });
